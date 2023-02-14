fb74a0563eeba61be833fc4e539152459931c526
csabamarton <martoncsab@gmail.com>
2023-02-14 11:19:49 +0100

    Initial commit for 3 microservices

---
+++ backend-cilent/src/main/java/com/csmarton/backendcilent/BackendCilentApplication.java
@@ -0,0 +1,13 @@
+package com.csmarton.backendcilent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCilentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCilentApplication.class, args);
+	}
+
+}
---
+++ backend-cilent/src/test/java/com/csmarton/backendcilent/BackendCilentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csmarton.backendcilent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCilentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
